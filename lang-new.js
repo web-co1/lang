6d5ba74d6c2f7cc344ae349281372bb9739327c9
Dev <86075203+web-co1@users.noreply.github.com>
2024-02-16 19:12:18 +0500

    Create lang-new.js

---
+++ lang-new.js
@@ -0,0 +1,348 @@
+var langmore__size_72 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f365870258ca8c18c459_Mask%20group%20(39).jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65afb5d582c75aa35f82aaa3_Mask%20group%20(14).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65afb69b8ed5453dedeac66d_Mask%20group%20(15).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65afb79a75569737dc5480d6_Mask%20group%20(16).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65afb7c27b14b2b6a6f2f5ab_Mask%20group%20(17).jpg",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f01fd9a795307399dbbb_Mask%20group%20(36).jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b10a2f7ed23201de20b600_replicate-prediction-rpujwxbbbzwmfudn5waflrpnxa%201%20(6).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1092751bd15cb61c43f3b_replicate-prediction-rpujwxbbbzwmfudn5waflrpnxa%201%20(5).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1093ae42df0d3ec15cab6_kitchen%202%20(22).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b10910129d87781b92f2a1_kitchen%202%20(21).jpg",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f081c31f17140841fd4f_Mask%20group%20(37).jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65bd01d71aac1d9f234e436a_kitchen%202%20(2).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b110d93ceaf2f573e4e681_green-drawer.jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b11101d4655faa9e975356_kitchen%202%20(24).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b10f2486b717747499c622_kitchen%202%20(23).jpg",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f14242f61308be39ffea_Mask%20group%20(38).jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b113f7d77b3a925aca6fa7_kitchen%202%20(25).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b11420c1d55528fa564b67_green-deg.jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b114277c1273a5ecdedd27_kitchen%202%20(26).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b11443f7e6ae4c57dbef33_replicate%20(2).jpg",
+    ],
+  },
+};
+
+var langmore__size_60 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f365870258ca8c18c459_Mask%20group%20(39).jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65afb5d582c75aa35f82aaa3_Mask%20group%20(14).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65afb69b8ed5453dedeac66d_Mask%20group%20(15).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65afb79a75569737dc5480d6_Mask%20group%20(16).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65afb7c27b14b2b6a6f2f5ab_Mask%20group%20(17).jpg",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f01fd9a795307399dbbb_Mask%20group%20(36).jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b10a2f7ed23201de20b600_replicate-prediction-rpujwxbbbzwmfudn5waflrpnxa%201%20(6).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1092751bd15cb61c43f3b_replicate-prediction-rpujwxbbbzwmfudn5waflrpnxa%201%20(5).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1093ae42df0d3ec15cab6_kitchen%202%20(22).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b10910129d87781b92f2a1_kitchen%202%20(21).jpg",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f081c31f17140841fd4f_Mask%20group%20(37).jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65bd01d71aac1d9f234e436a_kitchen%202%20(2).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b110d93ceaf2f573e4e681_green-drawer.jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b11101d4655faa9e975356_kitchen%202%20(24).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b10f2486b717747499c622_kitchen%202%20(23).jpg",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0f14242f61308be39ffea_Mask%20group%20(38).jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b113f7d77b3a925aca6fa7_kitchen%202%20(25).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b11420c1d55528fa564b67_green-deg.jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b114277c1273a5ecdedd27_kitchen%202%20(26).jpg",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b11443f7e6ae4c57dbef33_replicate%20(2).jpg",
+    ],
+  },
+};
+
+var langmore__size_48 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7ddbcdbecd59110d0877b_larg.png",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7ed95520bdac7821e676e_Mask%20group%20(26).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7ee17a3a3c789d057eeec_Mask%20group%20(29).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7edc56a31624b1c053b6f_Mask%20group%20(27).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7edf2cbe99ad7df071546_Mask%20group%20(28).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80ad96feee3c784d9334e_fourtw.jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80b91dd3cec0805a1b176_kitchen%202%20(5).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80bb57dddb51afe8d8427_Mask%20group%20(33).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80bdc562dec0ab6d8fd42_Mask%20group%20(34).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80c1fd64028c24aeab47a_fhse.png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81f88b512722649458ba3_repli.png",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81f88b512722649458ba3_repli.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b82244bdcc1ed9255b3263_rev.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b82283cec956305b282d72_kitchen%202%20(7).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b82346dee5f8308395eab6_reli%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8379669f68c98cafe3c61_replica.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b838393d872a0f99c26bf1_kitchen%202%20(9).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b8387f93a2e8fbba89418d_re.png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b8392419f6615078bc10d0_kitchen%202%20(10).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b839b6cf2982d305d1c791_greendrawer.png",
+    ],
+  },
+};
+
+var langmore__size_42 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7ddbcdbecd59110d0877b_larg.png",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7ed95520bdac7821e676e_Mask%20group%20(26).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7ee17a3a3c789d057eeec_Mask%20group%20(29).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7edc56a31624b1c053b6f_Mask%20group%20(27).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7edf2cbe99ad7df071546_Mask%20group%20(28).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80ad96feee3c784d9334e_fourtw.jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80b91dd3cec0805a1b176_kitchen%202%20(5).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80bb57dddb51afe8d8427_Mask%20group%20(33).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80bdc562dec0ab6d8fd42_Mask%20group%20(34).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80c1fd64028c24aeab47a_fhse.png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81f88b512722649458ba3_repli.png",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81f88b512722649458ba3_repli.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b82244bdcc1ed9255b3263_rev.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b82283cec956305b282d72_kitchen%202%20(7).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b82346dee5f8308395eab6_reli%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8379669f68c98cafe3c61_replica.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b838393d872a0f99c26bf1_kitchen%202%20(9).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b8387f93a2e8fbba89418d_re.png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b8392419f6615078bc10d0_kitchen%202%20(10).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b839b6cf2982d305d1c791_greendrawer.png",
+    ],
+  },
+};
+
+var langmore__size_36 = {
+  white: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b7c4372003df5bc2a3d546_thir.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65bd3ef8974812a59c99490e_kitchen%202%20(3).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7cb7253d868284c40d772_Mask%20group%20(23).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7cc09138156caaae09de9_Mask%20group%20(24).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7cc5316005dde312baa45_Mask%20group%20(25).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b800dae096d217959eae4c_mar.jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b801f062afcfd585affb0d_Mask%20group%20(30).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8024124221390a4bf3b89_Mask%20group%20(31).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8027c4ea625b1aced803f_Mask%20group%20(32).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80197e08b5c0254261c73_kitchen%202%20(4).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81921f24ef7e235bda696_repl.png",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81af740918140228f4684_rep2.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81b50b51272264942ae5d_Mask%20group%20(35).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81b8aad2fb9bbc5427aab_Mask%20group%20(36).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81aa748918891d7de5d93_kitchen%202%20(6).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b833a36521b1f787f96b9e_reli%20(2).png",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b834c60881b365b99fe77f_replicate-prediction-rpujwxbbbzwmfudn5waflrpnxa%201%20(2).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b834ec41a9511ce5f5ee58_kitchen%202%20(8).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b83566751553ed09759106_replicate-prediction-rpujwxbbbzwmfudn5waflrpnxa%201%20(3).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b833a36521b1f787f96b9e_reli%20(2).png",
+    ],
+  },
+};
+
+var langmore__size_30 = {
+  white: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65b7c4372003df5bc2a3d546_thir.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65bd3ef8974812a59c99490e_kitchen%202%20(3).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7cb7253d868284c40d772_Mask%20group%20(23).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7cc09138156caaae09de9_Mask%20group%20(24).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b7cc5316005dde312baa45_Mask%20group%20(25).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b800dae096d217959eae4c_mar.jpg",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b801f062afcfd585affb0d_Mask%20group%20(30).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8024124221390a4bf3b89_Mask%20group%20(31).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8027c4ea625b1aced803f_Mask%20group%20(32).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b80197e08b5c0254261c73_kitchen%202%20(4).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81921f24ef7e235bda696_repl.png",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81af740918140228f4684_rep2.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81b50b51272264942ae5d_Mask%20group%20(35).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81b8aad2fb9bbc5427aab_Mask%20group%20(36).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b81aa748918891d7de5d93_kitchen%202%20(6).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b833a36521b1f787f96b9e_reli%20(2).png",
+    sliders: [
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b834c60881b365b99fe77f_replicate-prediction-rpujwxbbbzwmfudn5waflrpnxa%201%20(2).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b834ec41a9511ce5f5ee58_kitchen%202%20(8).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b83566751553ed09759106_replicate-prediction-rpujwxbbbzwmfudn5waflrpnxa%201%20(3).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b833a36521b1f787f96b9e_reli%20(2).png",
+    ],
+  },
+};
+
+// Get references to size and color buttons
+const sizeButtons = document.querySelectorAll("#langmore .size-btn");
+const colorButtons = document.querySelectorAll("#langmore .variant-btn");
+
+// Function to update images based on selected size and color
+function updateImages(size, color) {
+  const sizeObject = window[`langmore__size_${size}`];
+  console.log("colr", color, size);
+
+  const colorObject = sizeObject[color];
+
+  if (sizeObject && colorObject) {
+    // Set main image URL
+    var mainImageElement = document.querySelector(
+      "#langmore .images-block img"
+    );
+    if (mainImageElement) {
+      mainImageElement.src = colorObject.mainImg;
+      mainImageElement.srcset = colorObject.mainImg;
+    }
+
+    // Loop through sliders and set new src attributes
+    var sliderImages = document.querySelectorAll(
+      "#langmore #langmore_slider .slide-item img"
+    );
+
+    if (colorObject.sliders.length > 0) {
+      for (var i = 0; i < sliderImages.length; i++) {
+        var index = i % colorObject.sliders.length;
+        var sliderImage = colorObject.sliders[index];
+
+        if (sliderImages[i]) {
+          sliderImages[i].src = sliderImage;
+          sliderImages[i].srcset = sliderImage;
+        }
+      }
+    }
+  }
+}
+
+// Function to handle size button click
+function handleSizeButtonClick(size) {
+  // Remove 'w--current' class from all size buttons
+  sizeButtons.forEach((button) => button.classList.remove("size__current"));
+
+  // Add 'w--current' class to the clicked size button
+  this.classList.add("size__current");
+
+  const Color = document.querySelector("#langmore .variant-btn.active")
+    .classList[2];
+
+  // Update images based on the selected size and default color
+  updateImages(size, Color);
+}
+
+// Function to handle color button click
+function handleColorButtonClick(color) {
+  // Remove 'active' class from all color buttons
+  colorButtons.forEach((button) => button.classList.remove("active"));
+
+  // Add 'active' class to the clicked color button
+  this.classList.add("active");
+
+  // Get the selected size button with 'w--current' class
+  const selectedSizeButton = document.querySelector(
+    "#langmore .size-btn.size__current"
+  );
+
+  // If a size button is selected, update images based on the selected size and color
+  if (selectedSizeButton) {
+    const selectedSize = selectedSizeButton.innerText.trim().split("”")[0]; //output 72"
+    updateImages(selectedSize, color);
+  }
+}
+
+// Attach click event listeners to size buttons
+sizeButtons.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleSizeButtonClick.call(this, this.innerText.trim().split("”")[0]);
+  })
+);
+
+// Attach click event listeners to color buttons
+colorButtons.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleColorButtonClick.call(this, this.classList[2]); // Assuming the color class is the second class in the classList
+  })
+);
